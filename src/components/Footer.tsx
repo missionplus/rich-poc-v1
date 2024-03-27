1b2fbde27edbea87ff839bddd4a7d471a6ca9cc7
Alvin Arulselvan <alvin@mission.plus>
2024-03-27 11:34:49 +0800

    Added updated footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,10 @@ const Footer: React.FC = () => {
   return (
     <footer style={footerStyle}>
       <p>© 2024 HelloRich Pte. Ltd. All rights reserved.</p>
-      <p>*Advice should be sought from a financial adviser regarding the suitability of the investment product, taking into account the specific investment objectives, financial situation or particular needs of any person in receipt of the recommendation, before the person makes a commitment to purchase the investment product.</p>
+      <p>*HelloRich.AI  provides information and advice for educational purposes only. HelloRich.AI does not offer advisory or brokerage services, nor does it recommend or advise investors to buy or sell particular insurance policies, stocks, securities or other investments.</p>
 <p>Should the person choose not to do so, he should consider carefully whether the product is suitable for him. In particular, all relevant documentations pertaining to the product should be read to make an independent assessment of the appropriateness of the transaction.</p>
-<p>The advertisement has not been reviewed by the Monetary Authority of Singapore, or any regulatory authority elsewhere.*</p>
+<p> It does not constitute an offer, recommendation, solicitation to enter into any transaction. You should seek advice from a licensed or an exempt financial adviser on the suitability of the product for you, taking into account these factors before making a commitment to purchase any product.</p>
+<p>Should the person choose not to do so,(s)he should consider carefully whether the product is suitable for them. The advertisement has not been reviewed by the Monetary Authority of Singapore, or any regulatory authority elsewhere.</p>
 <p>The data you have entered here will be used to train our bot.</p>
     </footer>
   );
